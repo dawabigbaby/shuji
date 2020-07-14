1cc48c8f9c48b8c548f824ba7e38577050985392
尚軼軒 <shangyixuan@sinosoft.hk>
2020-07-14 15:55:42 +0800

    创建项目

---
+++ src/main/java/com/dawaerwa/shuji/ShujiApplication.java
@@ -0,0 +1,13 @@
+package com.dawaerwa.shuji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShujiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShujiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dawaerwa/shuji/ShujiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawaerwa.shuji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShujiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
